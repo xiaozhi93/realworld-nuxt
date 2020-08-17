de74bf8b0123293ad56a7534391e4b809ed8f024
windfly <974589985@qq.com>
2020-08-17 18:17:08 +0800

    修改区分环境

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   apps : [{
-    name: 'realworld',
+    name: 'realworld-' + process.env.NODE_ENV,
     script: 'npm -- run start',
     watch: true,
     env: {
         "PORT": 3000,
         "NODE_ENV": "development"
     },
-    env_staging: {
-        "PORT": 8070,
-        "NODE_ENV": "production",
+    env_development: {
+        "PORT": 3000,
+        "NODE_ENV": "development",
     },
     env_production: {
         "PORT": 8080,
@@ -17,6 +17,20 @@ module.exports = {
     }
   }],
   deploy : {
+    development : {
+      user : 'root',
+      host : '119.45.51.253',
+      ref  : 'origin/development',
+      repo : 'https://gitee.com/xiaozhij/fed-e-task-03-03.git',
+      path : '/home/nuxt-realworld-development',
+      "env":{
+        "NODE_ENV":"development"
+      },
+      'pre-setup': 'rm -rf /home/nuxt-realworld-development/source',
+      "post-setup": "npm install && npm run build && pm2 start ecosystem.config.js --env development",
+      "pre-deploy": "echo '开始fetch项目然后重启应用'",
+      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env development',
+    },
     production : {
       user : 'root',
       host : '119.45.51.253',
